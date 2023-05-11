f234204fbd238687bdb874f7ac4e92a7653ad91d
BinZFAKE9999 <99895433+BinZFAKE9999@users.noreply.github.com>
2023-05-11 21:46:19 +0700

    Add files via upload

---
+++ bzot9-snake/newupdate.js
@@ -0,0 +1,12 @@
+function newUpdate1() {
+  alert("Version 3.0 Update: Delete GUI 2.0, Added GUI 3.0, add PWA Install, new UI for home page, more. Contact email: bzot9ws@gmail.com. Thanks!");
+}
+function newUpdate2() {
+  alert("Empty");
+}
+function newUpdate3() {
+  alert("Empty");
+}
+function newUpdate4() {
+  alert("Empty");
+}
